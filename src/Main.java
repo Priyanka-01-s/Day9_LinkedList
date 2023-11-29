2862d2874c4d840ff699566a7914615df192ae00
Priyanka Sengupta <78321696+Priyanka-01-s@users.noreply.github.com>
2023-11-30 01:45:59 +0530

    Adding elements at end

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,21 @@ public class Main {
 
         //create a simple Linked List of 56, 30 and 70
         Linkedlist<Integer> intLinkedlist = new Linkedlist<>();
+        Linkedlist<Integer> intLinkedlist2 = new Linkedlist<>();
 
         intLinkedlist.appendFirst(70);
         intLinkedlist.appendFirst(30);
         intLinkedlist.appendFirst(56);
 
         System.out.println("Adding elements in the beginning of the list:\n");
-        intLinkedlist.printList();        
+        intLinkedlist.printList();  
+        
+        intLinkedlist2.appendEnd(56);
+        intLinkedlist2.appendEnd(30);
+        intLinkedlist2.appendEnd(70);
+
+        System.out.println("\nAdding elements in the end of the list:\n");
+        intLinkedlist2.printList();  
 
     }
 }
